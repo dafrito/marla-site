e5252d2a6d7b3d34e344f97652aaf92554f53454
Aaron Faanes <dafrito@gmail.com>
2013-02-02 00:20:16 -0600

    Added several more utility subroutines

---
+++ lib/Marla/Response.pm
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+package Marla::Response;
+
+use strict;
+use warnings;
+
+use Carp;
+
+use Exporter;
+
+our @ISA     = qw(Exporter);
+our @EXPORT  = qw(response_200 response_404);
+our $VERSION = 1.00;
+
+sub response_404 {
+    my $request = shift;
+    my $path = shift;
+
+    return sub {
+        print "HTTP/1.1 404 Not found\r\n";
+        print $request->header;
+        print "File not found: $path";
+    };
+}
+
+sub response_200 {
+    my $request = shift;
+    my $content_builder = shift;
+
+    return sub {
+        my ($output, $media_type) = $content_builder->(@_);
+
+        print "HTTP/1.1 200 OK\r\n";
+        print $request->header($media_type);
+        print $output;
+    };
+}
+
+1;
+
+# vim: set ts=4 sw=4 :
--- lib/Marla/View.pm
+++ lib/Marla/View.pm
@@ -9,18 +9,40 @@ use Carp;
 require Exporter;
 
 our @ISA     = qw(Exporter);
-our @EXPORT  = qw();
+our @EXPORT  = qw(view_cat view_tt);
 our $VERSION = 1.00;
 
-sub cat {
-    my $file = shift or croak "No file specified";
+sub view_cat {
+    my $path = shift;
 
-    local($/);
-    open(my $fh, $file) or croak "Failed to serve static file: $file ($!)";
-    my $output = <$fh>;
-    close($fh);
+    -d $path or die "Path does not exist or is not a directory: $path";
 
-    return $output;
+    return sub {
+        my $file = shift or croak "No file specified";
+
+        local($/);
+        open(my $fh, "$path/$file") or croak "Failed to serve static file: $path/$file ($!)";
+        my $output = <$fh>;
+        close($fh);
+
+        return $output;
+    }
+}
+
+sub view_tt {
+    my $site = shift;
+    my $template_name = shift;
+
+    return sub {
+        my $output = "";
+
+        my $tt = $site->get_config("tt");
+
+        my $content = shift;
+        $tt->process($template_name, $content, \$output) or die($tt->error);
+
+        return $output;
+    };
 }
 
 1;
